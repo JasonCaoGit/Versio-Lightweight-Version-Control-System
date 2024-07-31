1ff7ad5545a2147c2ec2e94fbb213162f1528543
Jason Cao <jasonmmd6@gmail.com>
2024-07-31 15:48:35 -0400

    More fixes

--- gitlet/Repository.java
+++ gitlet/Repository.java
@@ -16,7 +16,7 @@ import java.util.Map;
  *  TODO: It's a good idea to give a description here of what else this Class
  *  does at a high level.
  *
- * @author TODO
+ * @author Zhuoyuan Cao
  */
 public class Repository {
     /**
